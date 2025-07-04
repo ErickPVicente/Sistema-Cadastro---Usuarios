a14f0578d00888835edb3c69d1f7a4e099ad9ddb
ErickPVicente <erickpachecovicente10@gmail.com>
2025-07-04 01:14:00 +0000

    reset

--- backend/controllers/authController.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/User');
-
-function validarEmail(email) {
-  return /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email);
-}
-function validarTelefone(telefone) {
-  return /^[0-9]{10,11}$/.test(telefone);
-}
